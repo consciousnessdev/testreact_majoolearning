8b876d6e2d6a059242af99f5ac57d5aa572f39ee
Ario Setiawan <consciousness.dev@gmail.com>
2022-03-04 21:02:35 +0700

    note: first test - change jest runner adding watch param - import/export in commonjs - Arrange, Act, and Assert

--- script.test.js
+++ script.test.js
@@ -1,3 +1,24 @@
+// commonjs import
+// arrange
+const googleSearch = require('./script');
+
+// mock db
+// arrange
+dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
+
 it('this is a test', () => {
+  //  googleSearch('testtest', dbMock);
   
+  //  matchers example, true value
+  //  expect('hello').toBe('hello');
+
+  // false value
+  // expect('hello').toBe('paman');
+});
+
+it('is searching google', () => {
+  // assert
+  expect(googleSearch('testtest', dbMock))
+    .toEqual([]); //assert
+  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
 });
